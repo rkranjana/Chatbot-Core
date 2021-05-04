693bd3492fcb95d505fefc1f235143c551a1437c
Ranjana R Krishnan <ranjana.r@experionglobal.local>
2021-05-04 13:40:54 +0530

    first commit

---
+++ Chatbotcore/ComponentList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbotcore
+{
+    public class ComponentList
+    {
+        public int index { get; set; }
+        public string name { get; set; }
+
+        public string choice { get; set; }
+    }
+}
---
+++ Chatbotcore/Forms/ComponentOrder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Chatbotcore.Forms
+{
+
+    public enum ComponentType { AutoComplete, Badge, Bottomsheet, Button, ButtonToggle, Card, Checkbox, Chips, Datepicker, Dialog, Divider, Expansionpanel, Formfield, Icon, Input, List, Menu, Paginator, Progressbar, Progressspinner, Radiobutton, Ripples, Select, Sidenav, Slidetoggle, Slider, Snackbar, Sortheader, Stepper, Table, Tabs, Toolbar, Tooltip };
+    
+    [Serializable]
+    public class ComponentOrder
+    {
+
+        
+
+
+
+        }
+}
